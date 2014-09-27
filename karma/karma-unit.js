058ef241ce1e4b8edf399fdb17e3a9b2bc5b3d6d
Ian Adams <ian@128K.co>
2014-09-27 16:56:59 -0700

    Created file for configuring karma unit tests

--- karma/karma-unit.js
+++ karma/karma-unit.js
@@ -1,12 +1,12 @@
 module.exports = function ( karma ) {
   karma.set({
-    /** 
-     * From where to look for files, starting with the location of this file.
+    /* 
+     * Where to look for files, starting from the location of this file.
      */
     basePath: '../',
 
-    /**
-     * This is the list of file patterns to load into the browser during testing.
+    /*
+     * The list of file patterns to load into the browser during testing.
      */
     files: [      
       'main.js',
@@ -16,30 +16,30 @@ module.exports = function ( karma ) {
     plugins: [ 'karma-jasmine', 'karma-chrome-launcher', 'karma-firefox-launcher', 'karma-safari-launcher' ],
     preprocessors: {},
 
-    /**
+    /*
      * How to report, by default.
      */
     reporters: 'dots',
 
-    /**
-     * On which port should the browser connect, on which port is the test runner
-     * operating, and what is the URL path for the browser to use.
+    /*
+     * Set which port the browser should connect to, which port the test runner
+     * operates on, and the URL path for the browser to use.
      */
     port: 9018,
     runnerPort: 9100,
     urlRoot: '/',
 
-    /** 
+    /* 
      * Disable file watching by default.
      */
     autoWatch: false,
 
-    /**
-     * The list of browsers to launch to test on.
-     * You may also leave this blank and manually navigate your browser to
-     * http://localhost:9018/ when you're running tests. The window/tab can be left
-     * open and the tests will automatically occur there during the build. This has
-     * the aesthetic advantage of not launching a browser every time you save.
+    /*
+     * The list of browsers to launch to test on. Manually navigating to
+     * http://localhost:9018/ to run tests is also possible. The window/tab can
+     * be left open and the tests will automatically occur there during the
+     * build. This has the aesthetic advantage of not launching a browser every
+     * time you save.
      */
     browsers: [
     	'Chrome',
@@ -48,4 +48,3 @@ module.exports = function ( karma ) {
     ]
   });
 };
-
